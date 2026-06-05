7822bf748a7e768cde9a05fd8234ce8413522e3f
HARI PAVAN INGILALA <161825246+Hari8328@users.noreply.github.com>
2026-06-05 13:31:13 +0530

    Implement LargestElement to find maximum in array

--- Arrays/LargestElement.java
+++ Arrays/LargestElement.java
@@ -1,1 +1,17 @@
+import java.util.*;
+public class LargestElement {
+    public static void main(String[] args) {
 
+        int[] arr = {10, 25, 8, 40, 15};
+
+        int largest = arr[0];
+
+        for(int i = 1; i < arr.length; i++) {
+            if(arr[i] > largest) {
+                largest = arr[i];
+            }
+        }
+
+        System.out.println("Largest Element = " + largest);
+    }
+}
